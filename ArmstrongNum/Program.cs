5d98873897e502be0eae13aca33aa74107328ad9
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-31 19:44:14 +0300

    Add project files.

---
+++ ArmstrongNum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you give the number maybe?");
+            string numstr = Console.ReadLine();
+            int total = ArmstrongNumbers(numstr);
+
+            if (total == int.Parse(numstr))
+            {
+                Console.WriteLine("That's an Armstrong Number!!");
+            } else
+            {
+                Console.WriteLine("That's not an Armstrong Number!!");
+            }
+            Console.ReadLine();
+        }
+        
+        static int ArmstrongNumbers(string numstr)
+        {
+            int num = int.Parse(numstr);
+            int total = 0;
+
+            for (int i = 0; i < numstr.Length; i++)
+            {
+                int digit = int.Parse(numstr[i].ToString());
+                total += (int)Math.Pow(digit, numstr.Length);
+            }
+
+            return total;
+        }
+       
+    }
+}
